63891b019ee08f478f97e481fa3ee4e5d56622a2
yumble <hnjn1101@gmail.com>
2023-03-08 11:02:49 +0900

    feat: 프로젝트 생성 및 gradle 설정
    - querydsl 설정
    - 쿼리파라미터 바인딩

---
+++ src/test/java/dorosee/initial/InitialApplicationTests.java
@@ -0,0 +1,13 @@
+package dorosee.initial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
