b5e5abaa6c65b0a7f81d6d27db90fbf8f1ef267b
yumble <hnjn1101@gmail.com>
2023-03-13 14:59:53 +0900

    fix: Fix 리턴 변수명 수정
    - result -> resultObject
    - results -> resultList

--- src/main/java/dorosee/initial/config/basestatus/BaseResponse.java
+++ src/main/java/dorosee/initial/config/basestatus/BaseResponse.java
@@ -9,26 +9,26 @@ public class BaseResponse<T> {
     private final Boolean isSuccess;
     private final String message;
     private final int code;
-    private T result;
-    private T results;
+    private T resultObject;
+    private T resultList;
     private Map<String, Object> totalMap;
 
     // 요청에 성공한 경우
-    public BaseResponse(T result, T results) {
+    public BaseResponse(T resultObject, T resultList) {
         this.isSuccess = BaseResponseStatus.SUCCESS.isSuccess();
         this.message = BaseResponseStatus.SUCCESS.getMessage();
         this.code = BaseResponseStatus.SUCCESS.getCode();
-        this.result = result;
-        this.results = results;
+        this.resultObject = resultObject;
+        this.resultList = resultList;
     }
 
     // 요청에 성공한 경우
-    public BaseResponse(T result, T results, Map<String, Object> totalMap) {
+    public BaseResponse(T resultObject, T resultList, Map<String, Object> totalMap) {
         this.isSuccess = BaseResponseStatus.SUCCESS.isSuccess();
         this.message = BaseResponseStatus.SUCCESS.getMessage();
         this.code = BaseResponseStatus.SUCCESS.getCode();
-        this.result = result;
-        this.results = results;
+        this.resultObject = resultObject;
+        this.resultList = resultList;
         this.totalMap = totalMap;
     }
 
